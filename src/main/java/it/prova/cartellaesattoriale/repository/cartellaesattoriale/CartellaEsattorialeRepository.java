ff3c0eb1d35a64329a4b10472ea72c39f843a3a3
Gabababab <gravottagabriele@gmail.com>
2021-11-19 15:05:13 +0100

    implementazione repository

---
+++ src/main/java/it/prova/cartellaesattoriale/repository/cartellaesattoriale/CartellaEsattorialeRepository.java
@@ -0,0 +1,10 @@
+package it.prova.cartellaesattoriale.repository.cartellaesattoriale;
+
+import org.springframework.data.repository.CrudRepository;
+
+import it.prova.cartellaesattoriale.model.CartellaEsattoriale;
+
+
+public interface CartellaEsattorialeRepository extends CrudRepository<CartellaEsattoriale, Long> {
+
+}
---
+++ src/main/java/it/prova/cartellaesattoriale/repository/contribuente/ContribuenteRepository.java
@@ -0,0 +1,9 @@
+package it.prova.cartellaesattoriale.repository.contribuente;
+
+import org.springframework.data.repository.CrudRepository;
+
+import it.prova.cartellaesattoriale.model.Contribuente;
+
+public interface ContribuenteRepository extends CrudRepository<Contribuente, Long> {
+
+}
